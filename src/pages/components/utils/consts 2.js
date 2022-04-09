a2c544ebfddf3eeda242b8739b3634214d36ef45
gingerFury7 <slavka2000@mail.ru>
2022-04-10 01:44:00 +0300

    new login page

---
+++ src/pages/components/utils/consts 2.js
@@ -0,0 +1,11 @@
+
+
+
+export const BLOG_ROUTE = '/blog'
+export const BLOG_SINGLE_ROUTE= '/blog-single'
+export const AWARD_ROUTE = '/award'
+export const CONTACT_ROUTE = '/contact'
+export const HOMEPAGE_ROUTE = '/home'
+export const AUTH_ROUTE = '/auth'
+export const REGISTRATION_ROUTE = '/registration'
+export const ADMIN_ROUTE = '/admin?panel'
