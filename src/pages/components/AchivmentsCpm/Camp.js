a943bb14c3f795527c78f67d54ab2df7f0d21102
gingerFury7 <slavka2000@mail.ru>
2022-04-11 13:38:56 +0300

    some new files

---
+++ src/pages/components/AchivmentsCpm/Camp.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export const Camp = () => {
+    // const [show, setShow] = useState(false);
+    return (
+        <section>
+            <div class="achivements-container">
+                <span class="achivements-title">Посещение лагеря:</span>
+                <span class="achivements-value">3</span>
+            </div>
+        </section>
+    )
+
+}
